d82e241968c1f279882a84f74794d7fdaf2dcbf0
haerutea <tszya2020@student.cis.edu.hk>
2019-01-21 20:06:29 +0800

    renamed from RankConstants

--- app/src/main/java/com/example/tszya2020/animalhelp/Constants.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/Constants.java
@@ -1,9 +1,10 @@
 package com.example.tszya2020.animalhelp;
 
-public class RankConstants {
+public class Constants {
 
     public static final String DEFAULT = "Default";
     public static final String HELPER = "Helper";
     public static final String PRO = "Professional";
+    public static final String UID_KEY = "uid";
 
 }
