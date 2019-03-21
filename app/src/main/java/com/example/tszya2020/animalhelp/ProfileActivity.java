30a51fec4f6746c83d00855bc33a2ee4607d0034
haerutea <tszya2020@student.cis.edu.hk>
2019-03-21 18:49:30 +0800

    added settings

--- app/src/main/java/com/example/tszya2020/animalhelp/ProfileActivity.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/ProfileActivity.java
@@ -16,11 +16,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class ProfileActivity extends AppCompatActivity
         implements View.OnClickListener
@@ -28,6 +31,7 @@ public class ProfileActivity extends AppCompatActivity
     private String logging = "profileActivity";
 
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
     private DatabaseReference userRef;
     private DatabaseReference allUsersDBRef;
     private DatabaseReference chatDBRef;
@@ -39,9 +43,11 @@ public class ProfileActivity extends AppCompatActivity
     private String bothUsersUid;
 
     //UI references
+    private TextView verification;
     private TextView username;
     private TextView email;
     private Button chat;
+    private Button settings;
     private Button logout;
 
     @Override
@@ -52,19 +58,21 @@ public class ProfileActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
 
+        verification = findViewById(R.id.profile_verification);
         username = findViewById(R.id.profile_username);
         email = findViewById(R.id.profile_email);
         chat = findViewById(R.id.chat_button);
+        settings = findViewById(R.id.settings_button);
         logout = findViewById(R.id.log_out_button);
 
         //EL - There is no user in the Database at this point. It only lives in FirebaseAuth, not FirebaseDatabase,
         // so if a user can't be found in the database, you would have to get the info from auth and save it in the database.
         //The code bellow always returns null for the user since its never saved to the database.
         userUid = getIntent().getStringExtra(Constants.UID_KEY);
-        userRef = FirebaseDatabase.getInstance().getReference().child(Constants.USER_PATH).child(userUid);
+        userRef = Constants.BASE_INSTANCE.child(Constants.USER_PATH).child(userUid);
         Log.d("userRef", userRef.toString());
         final TaskCompletionSource<String> getUserSource = new TaskCompletionSource<>();
-
+        
         userRef.addListenerForSingleValueEvent(new ValueEventListener()
             {
                 @Override
@@ -74,7 +82,6 @@ public class ProfileActivity extends AppCompatActivity
                     userAccount = dataSnapshot.getValue(User.class);
                     userAccount.setOnline(true);
                     getUserSource.setResult(null);
-
                 }
 
                 @Override
@@ -83,14 +90,25 @@ public class ProfileActivity extends AppCompatActivity
                     getUserSource.setException(databaseError.toException());
                     Log.d(logging, "onCancelled: " + databaseError.getMessage());
                 }
+
             });
 
+        //when user ref listener is done
         getUserSource.getTask().addOnCompleteListener(new OnCompleteListener<String>()
         {
             @Override
             public void onComplete(@NonNull Task<String> task) {
                 if(userAccount!= null)
                 {
+                    //show text according to verification status
+                    if(mAuth.getCurrentUser().isEmailVerified())
+                    {
+                        verification.setText(R.string.verified_email);
+                    }
+                    else
+                    {
+                        verification.setText(R.string.not_verified_email);
+                    }
                     username.setText(userAccount.getUsername());
                     email.setText(userAccount.getEmail());
                 }
@@ -103,6 +121,7 @@ public class ProfileActivity extends AppCompatActivity
         });
 
         chat.setOnClickListener(this);
+        settings.setOnClickListener(this);
         logout.setOnClickListener(this);
     }
 
@@ -114,6 +133,11 @@ public class ProfileActivity extends AppCompatActivity
         {
             Intent intent = new Intent(getApplicationContext(), ConnectActivity.class);
             intent.putExtra(Constants.CURRENT_USER_KEY, userAccount);
+            startActivity(intent);
+        }
+        else if(id == settings.getId())
+        {
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
             startActivity(intent);
         }
         else if(id == logout.getId())
