27193802b2f39c7da179e5847ec7e84501244710
haerutea <tszya2020@student.cis.edu.hk>
2019-03-26 08:52:35 +0800

    added change medication button

--- app/src/main/java/com/example/tszya2020/animalhelp/SettingsActivity.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/SettingsActivity.java
@@ -1,17 +1,19 @@
 package com.example.tszya2020.animalhelp;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener
 {
 
+    private FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
     private Button change_password;
+    private Button change_medication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,6 +22,12 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.settings_activity);
 
         change_password = findViewById(R.id.change_password_button);
+        change_medication = findViewById(R.id.change_medication_button);
+        change_medication.setEnabled(false);
+        if(!mUser.isEmailVerified())
+        {
+            change_password.setEnabled(false);
+        }
         change_password.setOnClickListener(this);
     }
 
@@ -29,7 +37,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         passFrag.show(this.getSupportFragmentManager(), "passwordFragment");
     }
 
-
     @Override
     public void onClick(View v)
     {
@@ -37,6 +44,10 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         if(i == change_password.getId())
         {
             openPassword();
+        }
+        else if(i == change_medication.getId())
+        {
+            //do nothing as of now
         }
     }
 }
