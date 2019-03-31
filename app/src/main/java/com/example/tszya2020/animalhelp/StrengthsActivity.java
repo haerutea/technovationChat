d047c4d4520c3737e479194800484946920261e5
haerutea <tszya2020@student.cis.edu.hk>
2019-04-01 00:10:57 +0800

    now requires user to choose at least 1 strength before signing up

---
+++ app/src/main/java/com/example/tszya2020/animalhelp/StrengthsActivity.java
@@ -0,0 +1,48 @@
+package com.example.tszya2020.animalhelp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StrengthsActivity extends AppCompatActivity
+{
+    private ArrayList<String> allStrengths;
+    private StrengthsAdapter strengthsAdapter;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.strengths_activity);
+
+        allStrengths = new ArrayList<>();
+        allStrengths.add(Constants.AGE_GROUP_DESC);
+        allStrengths.addAll(Arrays.asList(Constants.TARGET_AGE_GROUPS));
+        allStrengths.add(Constants.CATEGORY_DESC);
+        allStrengths.addAll(Arrays.asList(Constants.CATEGORY_GROUPS));
+        allStrengths.add(Constants.LANGUAGE_DESC);
+        allStrengths.addAll(Arrays.asList(Constants.LANGUAGE_GROUPS));
+        allStrengths.add("BUTTON");
+
+        recyclerView = findViewById(R.id.strengths_recycler_view);
+
+        strengthsAdapter = new StrengthsAdapter(allStrengths);
+
+        // vertical RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // adding inbuilt divider line
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(strengthsAdapter);
+
+    }
+}
