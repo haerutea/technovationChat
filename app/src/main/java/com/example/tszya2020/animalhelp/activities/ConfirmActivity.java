424546546df2ad3b9c9fbb68baa3141e9d729727
haerutea <tszya2020@student.cis.edu.hk>
2019-04-03 12:40:18 +0800

    added description to textField, added code to connect chat when accepted

--- app/src/main/java/com/example/tszya2020/animalhelp/activities/ConfirmActivity.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/activities/ConfirmActivity.java
@@ -1,5 +1,6 @@
 package com.example.tszya2020.animalhelp.activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,17 +9,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.tszya2020.animalhelp.UserSharedPreferences;
+import com.example.tszya2020.animalhelp.object_classes.Chat;
 import com.example.tszya2020.animalhelp.object_classes.Constants;
 import com.example.tszya2020.animalhelp.R;
 import com.example.tszya2020.animalhelp.object_classes.Request;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class ConfirmActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private String currentUid;
     private String logName;
+    private String requesterUid;
     private Request sentRequest;
+    private DatabaseReference requestRef;
+
     //UI fields
     private TextView tDescript;
     private Button bAccept;
@@ -30,10 +42,12 @@ public class ConfirmActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.confirm_activity);
         logName = "confirmingRequest";
+        currentUid = UserSharedPreferences.getInstance(ConfirmActivity.this).getStringInfo(Constants.UID_KEY);
         tDescript = findViewById(R.id.request_description);
         bAccept = findViewById(R.id.request_accept);
         bDeny = findViewById(R.id.request_deny);
 
+        setDescription();
         bAccept.setOnClickListener(this);
         bDeny.setOnClickListener(this);
 
@@ -41,64 +55,68 @@ public class ConfirmActivity extends AppCompatActivity implements View.OnClickLi
 
     private void setDescription()
     {
-        Constants.BASE_INSTANCE.child(Constants.REQUEST_PATH)
-                .addListenerForSingleValueEvent(new ValueEventListener()
+        final TaskCompletionSource<String> getRequestData = new TaskCompletionSource<>();
+        requestRef = Constants.BASE_INSTANCE.child(Constants.REQUEST_PATH)
+                .child(currentUid);
+        final ValueEventListener descriptListener = new ValueEventListener()
         {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
-                Log.d(logName, "requesterUid: " + dataSnapshot.getValue().toString());
-                //sentRequest = dataSnapshot.getValue(Request.class);
+                sentRequest = dataSnapshot.getValue(Request.class);
+                Log.d(logName, "request " + sentRequest);
+                getRequestData.setResult(null);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError)
             {
-                Log.d(logName, "onCancelled: " + databaseError.getMessage());
+
+            }
+        };
+        requestRef.addValueEventListener(descriptListener);
+
+        //when code finishes getting uid from database
+        getRequestData.getTask().addOnCompleteListener(new OnCompleteListener<String>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<String> task)
+            {
+                tDescript.setText(sentRequest.toString());
+                requestRef.removeEventListener(descriptListener);
             }
         });
+
     }
+
     @Override
     public void onClick(View v)
     {
         int id = v.getId();
         if(id == bAccept.getId())
         {
-            //connectChat();
+            connectChat();
         }
         else if(id == bDeny.getId())
         {
 
         }
     }
-/*
+
     private void connectChat()
     {
-        bothUsersUid = userAccount.getUid() + opposingUser.getUid();
-        Constants.BASE_INSTANCE.child(Constants.CHAT_PATH)
-                .addListenerForSingleValueEvent(new ValueEventListener()
-                {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-                    {
-                        newChatlog = new Chat(userAccount, opposingUser);
-                        FirebaseDatabase.getInstance().getReference().child(Constants.CHAT_PATH)
-                                .child(bothUsersUid).setValue(newChatlog);
-                        Log.d("toChatActivity", "fromConnectActivity");
-                        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
-                        //https://stackoverflow.com/a/2736612
-                        intent.putExtra(Constants.CURRENT_USER_KEY, userAccount);
-                        //TODO: FIGURE OUT IF ALL THESE IS NEEDED vvv
-                        intent.putExtra(Constants.UID_KEY, userAccount.getUid());
-                        intent.putExtra(Constants.OPPOSING_USER_KEY, opposingUser);
-                        intent.putExtra(Constants.CHAT_ROOM_ID_KEY, bothUsersUid);
-                        startActivity(intent);
-                    }
+        requestRef.removeValue();
+        String bothUsersUid = currentUid + requesterUid;
+        Chat newChatlog = new Chat(currentUid, requesterUid);
+        Constants.BASE_INSTANCE.child(Constants.CHAT_PATH);
+        FirebaseDatabase.getInstance().getReference().child(Constants.CHAT_PATH)
+                .child(bothUsersUid).setValue(newChatlog);
+        Log.d("toChatActivity", "fromConnectActivity");
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        //https://stackoverflow.com/a/2736612
+        intent.putExtra(Constants.CHAT_ROOM_ID_KEY, bothUsersUid);
+        intent.putExtra(Constants.CHAT_OBJECT_KEY, newChatlog);
+        startActivity(intent);
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError)
-                    {
-                    }
-                });
-    }*/
+    }
 }
