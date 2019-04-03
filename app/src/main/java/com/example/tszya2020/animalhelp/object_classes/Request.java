6fda091236ae2b6ea6620945d762741a8831d44f
haerutea <tszya2020@student.cis.edu.hk>
2019-04-03 12:48:37 +0800

    added uid in request, added empty constructor

--- app/src/main/java/com/example/tszya2020/animalhelp/object_classes/Request.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/object_classes/Request.java
@@ -4,13 +4,24 @@ import java.util.HashMap;
 
 public class Request
 {
+    private HashMap<String, Boolean> uid;
     private String username;
     private HashMap<String, String> preferences;
 
-    public Request(String inUsername, HashMap<String, String> inPreferences)
+    public Request()
     {
+    }
+
+    public Request(HashMap<String, Boolean> inUid, String inUsername, HashMap<String, String> inPreferences)
+    {
+        uid = inUid;
         username = inUsername;
         preferences = inPreferences;
+    }
+
+    public HashMap<String, Boolean> getUid()
+    {
+        return uid;
     }
 
     public String getUsername()
@@ -21,5 +32,13 @@ public class Request
     public HashMap<String, String> getPreferences()
     {
         return preferences;
+    }
+
+    @Override
+    public String toString()
+    {
+
+        return username + " has requested to chat with you.  Here are the preferences chosen: "
+                + preferences.toString();
     }
 }
