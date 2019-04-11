ce3d220ceca86b4d3e1338bd64154746d27bce10
haerutea <tszya2020@student.cis.edu.hk>
2019-04-11 11:22:36 +0800

    deleting sendTokenToServer and sendNotif helper methods

--- app/src/main/java/com/example/tszya2020/animalhelp/FirebaseNotificationMessaging.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/FirebaseNotificationMessaging.java
@@ -22,24 +22,6 @@ public class FirebaseNotificationMessaging extends FirebaseMessagingService
     // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages
-        // are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data
-        // messages are the type
-        // traditionally used with GCM. Notification messages are only received here in
-        // onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated
-        // notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages
-        // containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always
-        // sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(logTag, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
@@ -50,18 +32,14 @@ public class FirebaseNotificationMessaging extends FirebaseMessagingService
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(logTag, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(notifTitle, remoteMessage.getNotification().getBody());
+            NotificationSender.setNotif(this, ConfirmActivity.class,
+                    notifTitle, remoteMessage.getNotification().getBody(), true);
         }
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        //TY - implemented it above
-
     }
     // [END receive_message]
 
 
     // [START on_new_token]
-
     /**
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the InstanceID token
@@ -73,20 +51,6 @@ public class FirebaseNotificationMessaging extends FirebaseMessagingService
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(token);
-    }
-    // [END on_new_token]
-
-    /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM InstanceID token with any server-side account
-     * maintained by your application.
-     *
-     * @param token The new token.
-     */
-    private void sendRegistrationToServer(String token)
-    {
         String uid = getSharedPreferences(Constants.PREF_USER_INFO, MODE_PRIVATE)
                 .getString(Constants.UID_KEY, "");
         //change token value in database
@@ -95,16 +59,5 @@ public class FirebaseNotificationMessaging extends FirebaseMessagingService
                     .child(Constants.TOKEN_KEY).child(token).setValue(true);
         //change token value in sharedPref
         UserSharedPreferences.getInstance(this).setInfo(Constants.TOKEN_KEY, token);
-    }
-
-    /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
-     */
-    private void sendNotification(String messageTitle, String messageBody)
-    {
-        NotificationSender.setNotif(this, ConfirmActivity.class,
-                messageTitle, messageBody, true);
     }
 }
