c2667e153d91a8832555045835428379b8c9c7ab
haerutea <tszya2020@student.cis.edu.hk>
2019-04-15 18:32:14 +0800

    added comments and javadocs

--- app/src/main/java/com/example/tszya2020/animalhelp/DialogUtils.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/DialogUtils.java
@@ -3,10 +3,19 @@ package com.example.tszya2020.animalhelp;
 import android.app.ProgressDialog;
 import android.content.Context;
 
+/**
+ * utils class to create and show ProgressDialogs easier
+ */
 public class DialogUtils {
 
-
     //https://stackoverflow.com/a/37428936
+
+    /**
+     * creates and shows new progress dialog, then returns the object so it can be dismissed.
+     * @param context context of where it'll show up
+     * @param message message to be shown on dialog
+     * @return the created ProgressDialog object so it can be dismissed
+     */
     public static ProgressDialog showProgressDialog(Context context, String message)
     {
         ProgressDialog progress = new ProgressDialog(context);
