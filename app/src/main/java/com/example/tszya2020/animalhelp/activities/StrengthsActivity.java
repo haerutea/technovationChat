d5f598860f975ec9bb71d01a21bd6db3938b6877
haerutea <tszya2020@student.cis.edu.hk>
2019-04-15 16:07:22 +0800

    added comments and javadocs

--- app/src/main/java/com/example/tszya2020/animalhelp/activities/StrengthsActivity.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/activities/StrengthsActivity.java
@@ -1,5 +1,7 @@
 package com.example.tszya2020.animalhelp.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -19,12 +21,20 @@ public class StrengthsActivity extends AppCompatActivity
     private StrengthsAdapter strengthsAdapter;
     private RecyclerView recyclerView;
 
+    /**
+     * when activity is first opened, set content from strengths_activity.xml,
+     * call show disclaimer to show dialog window, then add contents of recycler view
+     * to field, assign view to field, create new StrengthsAdapter with the contents,
+     * create layout manager and set adapter.
+     * @param savedInstanceState data saved from onSaveInstanceState, not used
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.strengths_activity);
 
+        showDisclaimer();
         allStrengths = new ArrayList<>();
         allStrengths.add(Constants.AGE_GROUP_DESC);
         allStrengths.addAll(Arrays.asList(Constants.TARGET_AGE_GROUPS));
@@ -45,6 +55,30 @@ public class StrengthsActivity extends AppCompatActivity
         // adding inbuilt divider line
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setAdapter(strengthsAdapter);
+    }
 
+    /**
+     *  shows diaclaimer dialog and force user to press 'I agree' button before proceeding
+     */
+    private void showDisclaimer()
+    {
+        //https://developer.android.com/guide/topics/ui/dialogs.html#java
+        String disclaimer = "This app isn\'t tended for emergency purposes, but rather simple and " +
+                "emotionally-easing chats with anonymous strangers.  If you're in a critical/dangerous" +
+                "situation, you should call emergency services or close ones to seek guidance instead.";
+        AlertDialog.Builder disclaimerBuilder = new AlertDialog.Builder(this);
+        disclaimerBuilder.setMessage(disclaimer)
+                .setTitle("PLEASE READ THIS")
+                .setMessage(disclaimer)
+                .setNeutralButton("I agree", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        //nothing bcs user has to click on this to continue
+                    }
+                }).setCancelable(false)
+                .create()
+                .show();
     }
 }
