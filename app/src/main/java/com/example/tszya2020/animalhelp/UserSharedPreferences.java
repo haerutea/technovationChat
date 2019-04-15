b0a098ded84bcc3971d3b8505403a8d3dffc6988
haerutea <tszya2020@student.cis.edu.hk>
2019-04-15 19:27:27 +0800

    added comments and javadocs

--- app/src/main/java/com/example/tszya2020/animalhelp/UserSharedPreferences.java
+++ app/src/main/java/com/example/tszya2020/animalhelp/UserSharedPreferences.java
@@ -8,6 +8,9 @@ import com.example.tszya2020.animalhelp.object_classes.Constants;
 // referenced from:
 // https://github.com/hieuapp/android-firebase-chat/blob/master/RivChat/app/src/main/java/com/android/rivchat/data/SharedPreferenceHelper.java
 
+/**
+ * own class to handle SharedPreferences to save and get info
+ */
 public class UserSharedPreferences
 {
     //https://stackoverflow.com/a/5950109
@@ -15,10 +18,19 @@ public class UserSharedPreferences
     private static SharedPreferences sharedPreferences;
     private static SharedPreferences.Editor editor;
 
+    /**
+     * needed empty constructor
+     */
     public UserSharedPreferences()
     {
     }
 
+    /**
+     * acts like a constructor, creates and returns instance of this class,
+     * also sets sharedPreferences and editor fields
+     * @param context context of where this is called
+     * @return new instance of UserSharedPreferences.
+     */
     public static UserSharedPreferences getInstance(Context context)
     {
         if(prefInstance == null)
@@ -30,19 +42,24 @@ public class UserSharedPreferences
         return prefInstance;
     }
 
+    /**
+     * puts string into editor and applies it to make changes and add data to sharedpreferences.
+     * @param key String, key of info
+     * @param data String, value of info
+     */
     public void setInfo(String key, String data)
     {
         editor.putString(key, data);
         editor.apply();
     }
 
+    /**
+     * gets the String with the corresponding key, default value is an empty string
+     * @param key String, key of needed String.
+     * @return value with the corresponding key
+     */
     public String getStringInfo(String key)
     {
         return sharedPreferences.getString(key, "");
-    }
-
-    public boolean getBoolInfo(String key)
-    {
-        return sharedPreferences.getBoolean(key, true);
     }
 }
